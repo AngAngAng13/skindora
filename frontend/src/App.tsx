d8fea838b5eef28687fa4b817a2e14b52900536f
Nguyen Tuan An <tuanan298@icloud.com>
2025-05-19 21:46:32 +0700

    Set up frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>vy senior 100 nam kinh nghiem</Button>
+    </div>
+  );
+}
+
+export default App;
