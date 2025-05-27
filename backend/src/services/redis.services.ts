6c8374190050db352af2453e7af27f18da8c526d
Phan Văn Trường <phantryong@gmail.com>
2025-05-27 22:43:57 +0700

    Modified Redis Configuration

--- backend/src/services/redis.services.ts
+++ backend/src/services/redis.services.ts
@@ -1,22 +1,27 @@
 import { createClient } from 'redis'
 import { config } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 
 config()
-const host: string = process.env.HOST_IP || ''
-const port: number = Number(process.env.HOST_REDIS_PORT) || 6379
+const host: string = process.env.HOST_NAME || 'localhost'
+const port: number = Number(process.env.HOST_REDIS_PORT) || 443
+const caCertPath = path.resolve(__dirname, '..', '..', 'certs', 'ca.crt') // Adjust '..' based on your project structure
 
 const redisClient = createClient({
   socket: {
     host,
-    port
+    port,
+    tls: true,
+    servername: 'redis.skindora.site',
+    ca: [fs.readFileSync(caCertPath, 'utf-8')]
   },
-  password: process.env.HOST_PASSWORD || ''
+  password: process.env.HOST_PASSWORD
 })
 
 redisClient.on('error', (err) => {
   console.log('Redis connect error: ', err)
 })
-
 ;(async () => {
   await redisClient.connect()
 })() // connect redis ngay lập tức
