83c3ebd6815a9279e290cd7175add9dbc9509d2f
Truongle68 <truonglephuc11@gmail.com>
2025-05-29 23:45:50 +0700

    Build start chat event

--- backend/src/lib/socket.ts
+++ backend/src/lib/socket.ts
@@ -38,6 +38,66 @@ io.on('connection', (socket) => {
   userSocketMap[userId] = socket.id
 
   console.log(`[${role}] ${userId} connected`)
+
+  //emit online staff khi có staff connected
+  if(role === 'staff'){
+    onlineStaffIds = Object.keys(userSocketMap).filter((id)=>{
+      const userSocket = io.sockets.sockets.get(userSocketMap[id])
+      return userSocket?.data.role === 'staff'
+    })
+    io.emit('getOnlineStaff', onlineStaffIds)
+  }
+
+  //start chat event
+  //customer là người start chat
+  socket.on('startChat', ()=>{
+    if(role !== 'customer') return
+    if(socket.data.pairedUserId){
+      console.log(`Customer ${userId} is already in a chat with ${socket.data.pairedUserId}`)
+      return
+    }
+    //chọn ngẫu nhiên 1 staff online
+    const staffId = pickAvailableStaff()
+    if(!staffId){
+      console.log("No available staff to chat")
+      return
+    }
+    console.log(`Customer ${userId} is paired with staff ${staffId}`)
+    const staffSocket = io.sockets.sockets.get(userSocketMap[staffId])
+    if (!staffSocket || staffSocket.data.role !== 'staff') {
+      console.log(`Staff ${staffId} is not available`)
+      return
+    }
+
+    socket.data.pairedUserId = staffId
+    //Gán pairedUserId cho staffSocket
+    staffSocket.data.pairedUserId = socket.data.userId
+    //Gán customerId vào list nếu chưa có
+    staffSocket.data.pairedCustomerIds = staffSocket.data.pairedCustomerIds || []
+    if (!staffSocket.data.pairedCustomerIds.includes(socket.data.userId)) {
+      staffSocket.data.pairedCustomerIds.push(socket.data.userId)
+    }
+
+    socket.emit('chatStarted', staffId)
+    staffSocket.emit('chatStarted', socket.data.userId)
+  })
 })
+
+function pickAvailableStaff(): string | null {
+  if(onlineStaffIds.length === 0){
+    console.log("No available staff online")
+    return null
+  }
+
+  //Chọn staff đang paired với ít customer nhất
+  const sortStaffList = onlineStaffIds.sort((a,b)=>{
+    const aSocket = io.sockets.sockets.get(userSocketMap[a])
+    const bSocket = io.sockets.sockets.get(userSocketMap[b])
+    return (aSocket?.data.pairedCustomerIds?.length || 0) - (bSocket?.data.pairedCustomerIds?.length || 0)
+  })
+
+  //Chọn staff đầu tiên
+  return sortStaffList[0] || null
+}
 
 export { io, server, app }
