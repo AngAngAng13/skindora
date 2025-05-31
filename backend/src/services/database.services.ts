cbc39bd856ef0802713e279b0b17058f8cd125ed
Truongle68 <truonglephuc11@gmail.com>
2025-05-31 17:05:24 +0700

    Add getter accessor

--- backend/src/services/database.services.ts
+++ backend/src/services/database.services.ts
@@ -2,6 +2,9 @@ import { MongoClient, Db, Collection } from 'mongodb'
 import { config } from 'dotenv'
 import User from '~/models/schemas/User.schema'
 import RefreshToken from '~/models/schemas/RefreshToken.schemas'
+import Product from '~/models/schemas/Products/Product.schema'
+import Order from '~/models/schemas/Orders/Order.schema'
+import OrderDetail from '~/models/schemas/Orders/OrderDetail.schema'
 
 config()
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@skindora.rbbhqia.mongodb.net/?retryWrites=true&w=majority&appName=skindora`
@@ -27,6 +30,18 @@ class DatabaseService {
 
   get users(): Collection<User> {
     return this.db.collection(process.env.DB_USERS_COLLECTION as string)
+  }
+
+  get products(): Collection<Product>{
+    return this.db.collection(process.env.DB_PRODUCTS_COLLECTION as string)
+  }
+
+  get orders(): Collection<Order>{
+    return this.db.collection(process.env.DB_ORDERS_COLLECTION as string)
+  }
+
+  get orderDetails(): Collection<OrderDetail>{
+    return this.db.collection(process.env.DB_ORDER_DETAIL_COLLECTION as string)
   }
 
   async indexUsers() {
