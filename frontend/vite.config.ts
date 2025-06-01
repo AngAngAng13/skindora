842373dda50e5b91ef74d502cddbbf3de42a90de
Nguyen Tuan An <tuanan298@icloud.com>
2025-06-01 21:46:50 +0700

    feat: add Sentry integration for error tracking in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,7 +6,10 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), sentryVitePlugin({
+    org: "fpt-d1",
+    project: "javascript-react"
+  })],
   build: {
     sourcemap: true,
   },
