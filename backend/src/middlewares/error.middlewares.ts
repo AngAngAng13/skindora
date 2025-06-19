ed6b1fadadb4a1d7c65bfa67d9954c43a7b4ab6f
Phan Văn Trường <phantryong@gmail.com>
2025-06-19 21:26:03 +0700

    Add defaltErrorHadler to handle EntityError

--- backend/src/middlewares/error.middlewares.ts
+++ backend/src/middlewares/error.middlewares.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction } from 'express'
 import { omit } from 'lodash'
 import HTTP_STATUS from '~/constants/httpStatus'
-import { ErrorWithStatus } from '~/models/Errors'
+import { EntityError, ErrorWithStatus } from '~/models/Errors'
 
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof EntityError) {
+    const firstKey = Object.keys(err.errors)[0]
+    const firstMessage = err.errors[firstKey]?.msg || 'Validation error'
+
+    res.status(HTTP_STATUS.UNPROCESSABLE_ENTITY).json({
+      message: firstMessage
+    })
+    return
+  }
+
   if (err instanceof ErrorWithStatus) {
     res.status(err.status).json(omit(err, ['status']))
-    return 
+    return
   }
   Object.getOwnPropertyNames(err).forEach((key) => {
     Object.defineProperty(err, key, { enumerable: true })
